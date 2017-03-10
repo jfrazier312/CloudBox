0dd6542e87152483aab75b81495b6a61b2520cc1
Jordan Frazier <jrf30@duke.edu>
2017-03-10 04:53:47 -0500

    Clean up user controller and add delete functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all.paginate(page: params[:page], per_page: 10)
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -36,16 +28,13 @@ class UsersController < ApplicationController
 
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-
     if user_params[:password].blank? && !current_user?(@user)
       user_params.delete(:password)
       user_params.delete(:password_confirmation)
     end
 
-    if @user.update_attributes(user_params)
+    if @user.update(user_params)
       flash[:success] = "User updated successfully"
       redirect_to @user
     else
@@ -54,11 +43,13 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user.destroy
-    redirect_to posts_path
+    if @user.destroy
+       flash[:success] = "User deleted"
+    else
+      flash.now[:danger] = "user cannot be deleted"
+    end
+    redirect_to users_path
   end
 
   private
